e1a73c94c3f0e412aa7fcd86dc0fa667813cbcc2
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2023-08-25 19:46:50 +0530

    JSON.js

--- Object&JSON/JSON.js
+++ Object&JSON/JSON.js
@@ -1,8 +1,9 @@
-// Javascript Object Notation
+// Javascript Object Notation(JSON)
 // JSON keys always is " " - in string fromat
 //JSON cant have functions *****
 //converting js object to json format strings
 
+// Normal Object Created
 let jsObj = {
     name : 'Akshay',
     age : 23,
@@ -12,11 +13,19 @@ let jsObj = {
     }
 }
 
+// Json Object
+let person ={
+    "name" : "akshay",
+     "age" : 23,
+    "work" : "Full-Stack Developer"
+}
+console.log(person); // here output will show!
+
 //conver js to JSON
 const JSONobj = JSON.stringify(jsObj);
 console.log(JSONobj)
 
-//convert JSON to js
+//convert JSON to js(javascript)
 let javaS = JSON.parse(JSONobj)
 console.log(javaS);
 
